1275c9a7bd71a72e78389c356b27e63aa9e1d53b
cricket <rakhaevpavel@gmail.com>
2021-10-17 18:50:31 +0300

    Добавлена логика на фильтрацию по названию курса в компонент CourseList

--- resources/store/courseList/selectors.js
+++ resources/store/courseList/selectors.js
@@ -1,3 +1,4 @@
 export const selectCourseList = state => state.courseList.courseList;
 export const selectCourseListLength = state => state.courseList.courseList.length;
 export const selectSortedCourseList = state => state.courseList.courseList.filter((course, index) => index < 5);
+export const selectIsFiltered = state => state.courseList.courseList.isFiltered;
