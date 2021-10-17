b045b5f44329daf653bd15ab139f9ed3ec53f6b5
cricket <rakhaevpavel@gmail.com>
2021-10-17 18:02:26 +0300

    Добавлена логика для компонента CourseFilter

--- resources/js/components/Example.js
+++ resources/js/components/Example.js
@@ -5,12 +5,14 @@ import { PersistGate } from 'redux-persist/integration/react';
 import { CourseList } from './CourseList/CourseList';
 import { store } from '../../store';
 import { persistor } from '../../store';
-import '../../css/app.css'
+import '../../css/app.css';
+import { CourseFilter } from './CourseFilter/CourseFilter';
 
 function Example () {
     return (
         <Provider store={store}>
             <PersistGate persistor={persistor}>
+                <CourseFilter/>
                 <CourseList/>
             </PersistGate>
         </Provider>
