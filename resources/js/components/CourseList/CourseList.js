edb3c4ce8b73107b573d87b74c613bc8fa6bc4dc
cricket <rakhaevpavel@gmail.com>
2021-10-17 21:56:22 +0300

    Доработана логика фильтрации CourseList

--- resources/js/components/CourseList/CourseList.js
+++ resources/js/components/CourseList/CourseList.js
@@ -1,6 +1,6 @@
 import { useCallback, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { selectCourseList, selectIsFiltered } from '../../../store/courseList/selectors';
+import { selectCourseList, selectFilteredList, selectIsFiltered } from '../../../store/courseList/selectors';
 import { Course } from '../Course/Course';
 import { courseListFilter, getCourseList } from '../../../store/courseList/action';
 import { CourseFilter } from '../CourseFilter/CourseFilter';
@@ -8,7 +8,9 @@ import style from './CourseList.module.scss';
 
 export const CourseList = () => {
     const courseList = useSelector(selectCourseList);
+    const filteredList = useSelector(selectFilteredList);
     const isFiltered = useSelector(selectIsFiltered);
+    console.log(isFiltered);
     const dispatch = useDispatch();
 
     const handleFilter = useCallback((value) => {
@@ -25,7 +27,10 @@ export const CourseList = () => {
                 <h2 className={style.section__title}>Каталог курсов</h2>
                 <CourseFilter onSubmit={handleFilter}/>
                 <div className={style.section__list}>
-                    {courseList.map(course => <Course item={course} key={course.id}/>)}
+                    {isFiltered ?
+                        filteredList.map(course => <Course item={course} key={course.id}/>) :
+                        courseList.map(course => <Course item={course} key={course.id}/>)
+                    }
                 </div>
             </div>
         </section>
