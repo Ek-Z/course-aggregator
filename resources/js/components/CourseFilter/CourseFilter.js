fa37a2762f5dc21c8a0515368b36f9cc2a8d8bd1
VFray <rekrut313@gmail.com>
2021-10-18 14:45:59 +0300

    filters base
    1. Install icons-material
    2. Add checkboxes

--- resources/js/components/CourseFilter/CourseFilter.js
+++ resources/js/components/CourseFilter/CourseFilter.js
@@ -1,23 +1,79 @@
-import { useState } from 'react';
 import style from './CourseFilter.module.scss';
 
-export const CourseFilter = ({ onSubmit }) => {
-    const [value, setValue] = useState('');
+import Accordion from '@mui/material/Accordion';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import Typography from '@mui/material/Typography';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
-    const handleChange = (evt) => {
-        setValue(evt.target.value);
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { selectFilters } from '../../../store/courseList/selectors';
+
+export const CourseFilter = ({ onSubmit }) => {
+    const filters = useSelector(selectFilters);
+    const obj = {};
+    filters.progLangs.forEach((lang) => (obj[lang] = false));
+    const [langFilters, setLangFilters] = useState(obj);
+    // const [value, setValue] = useState('');
+
+    // const handleChange = (evt) => {
+    //     setValue(evt.target.value);
+    // };
+
+    const handleLangChange = (filter) => {
+        setLangFilters((prev) => {
+            const newFilters = { ...prev };
+            newFilters[filter] = !newFilters[filter];
+            console.log(newFilters);
+            return newFilters;
+        });
     };
 
     const handleSubmit = (evt) => {
         evt.preventDefault();
-        onSubmit(value);
-        setValue('');
+        // onSubmit(value);
+        // setValue('');
+        // console.log(evt.target);
+        console.log(langFilters);
     };
 
     return (
-        <form action="#" onSubmit={handleSubmit}>
-            <input type="text" value={value} onChange={handleChange} />
-            <button type="submit">Поиск</button>
-        </form>
+        <div className={style.filter__container}>
+            <form action="#" onSubmit={handleSubmit}>
+                {/* <input type="text" value={value} onChange={handleChange} /> */}
+                <Accordion>
+                    <AccordionSummary
+                        expandIcon={<ExpandMoreIcon />}
+                        aria-controls="panel1bh-content"
+                        id="panel1bh-header"
+                    >
+                        <Typography sx={{ flexShrink: 0 }}>Языки программирования</Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <FormGroup>
+                            {filters.progLangs?.map((filter) => (
+                                <FormControlLabel
+                                    key={filter}
+                                    control={
+                                        <Checkbox
+                                            checked={langFilters[filter]}
+                                            onChange={() => handleLangChange(filter)}
+                                        />
+                                    }
+                                    label={filter}
+                                />
+                            ))}
+                            {/* <FormControlLabel control={<Checkbox />} label="PHP" /> */}
+                            {/* <FormControlLabel control={<Checkbox />} label="JavaScript" /> */}
+                        </FormGroup>
+                    </AccordionDetails>
+                </Accordion>
+                <button type="submit">Поиск</button>
+            </form>
+        </div>
     );
 };
