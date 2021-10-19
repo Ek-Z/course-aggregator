c787abb833ce3e4a2bb0784cecf303a491e62460
VFray <rekrut313@gmail.com>
2021-10-19 22:35:08 +0300

    Рефакторинг
    1. Переместил store в папку resources/js
    2. Переименовал Example.js в App.js
    3. Поменял наименования при подключении фронта
    4. Переименовал CourseFilter.js в Catalog.js
    5. Изменил верстку Catalog.js и Course.js под MaterialUI

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,15 +1,32 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes React and other helpers. It's a great starting point while
- * building robust, powerful web applications using React + Laravel.
- */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Catalog } from './layouts/Catalog/Catalog';
+import { store } from './store';
+import { persistor } from './store';
+import '../css/app.css';
+import { StyledEngineProvider } from '@mui/material/styles';
+import { Header } from './layouts/Header/Header';
 
-// require('./bootstrap');
+function App() {
+    return (
+        <Provider store={store}>
+            <PersistGate persistor={persistor}>
+                <Header />
+                <Catalog />
+            </PersistGate>
+        </Provider>
+    );
+}
 
-/**
- * Next, we will create a fresh React component instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+export default App;
 
-require('./components/Example');
+if (document.getElementById('app')) {
+    ReactDOM.render(
+        <StyledEngineProvider injectFirst>
+            <App />
+        </StyledEngineProvider>,
+        document.getElementById('app')
+    );
+}
--- resources/js/components/Course/Course.js
+++ resources/js/components/Course/Course.js
@@ -1,27 +1,24 @@
 //import style from './Course.module.scss'
-import { Typography,CardMedia,CardContent,Card} from '@mui/material';
+import { Typography, CardMedia, CardContent, Card, Grid } from '@mui/material';
 
-export const Course = ({item}) => {
+export const Course = ({ item }) => {
     return (
-        <Card sx={{ maxWidth: 500 }}>
-            <CardMedia
-            component="img"
-            height={140}
-            image="" alt="Course-image"
-            alt="green iguana"
-            />
-            <CardContent>
-                <Typography variant="h5">
-                    {item.title}    
-                </Typography>
-                <Typography variant="body2">
-                    {item.description}
-                </Typography>
-                <Typography variant="h6">
-                    {item.author || 'Имя пользователя'}
-                </Typography>
-            </CardContent>
-        </Card>
+        <Grid item xs={5}>
+            <Card sx={{ maxWidth: 500, height: '100%', display: 'flex', flexDirection: 'column' }}>
+                <CardMedia component="img" height={140} image="" alt="Course-image" alt="green iguana" />
+                <CardContent sx={{ flex: 1, display: 'flex', flexDirection: 'column', gap: '1rem' }}>
+                    <Typography variant="h5">{item.title}</Typography>
+                    <Typography
+                        sx={{ maxHeight: '5rem', textOverflow: 'ellipsis', overflow: 'hidden' }}
+                        variant="body2"
+                    >
+                        {item.description}
+                    </Typography>
+                    <Typography sx={{ display: 'block', marginTop: 'auto', alignSelf: 'flex-end' }} variant="h6">
+                        {item.author || 'Имя пользователя'}
+                    </Typography>
+                </CardContent>
+            </Card>
+        </Grid>
     );
 };
-             
