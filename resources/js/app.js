4069682787188f9e85105d1c0465772cb74f8a75
cricket <rakhaevpavel@gmail.com>
2021-10-23 18:14:11 +0300

    Тема перенесена из App.js в отдельный файл

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,30 +3,14 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { Switch, Route, HashRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { ThemeProvider } from '@mui/material/styles';
 import { Catalog } from './layouts/Catalog/Catalog';
 import { store } from './store';
+import { theme } from '../theme/theme';
 import { persistor } from './store';
 import { Header } from './layouts/Header/Header';
 import { AdminPanel } from './layouts/AdminPanel/AdminPanel';
 import '../css/app.css';
-
-const theme = createTheme({
-    palette: {
-        primary: {
-            light: '#757ce8',
-            main: '#3f50b5',
-            dark: '#002884',
-            contrastText: '#fff',
-        },
-        secondary: {
-            light: '#ff7961',
-            main: '#f44336',
-            dark: '#ba000d',
-            contrastText: '#000',
-        },
-    },
-});
 
 function App () {
     return (
