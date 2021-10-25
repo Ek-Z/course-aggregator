2fae6e07223f5eed261ad7433798970d626ae06d
cricket <rakhaevpavel@gmail.com>
2021-10-26 00:22:03 +0300

    Добавлен action для CourseCard.js

--- resources/js/components/CourseCard/CourseCard.js
+++ resources/js/components/CourseCard/CourseCard.js
@@ -1,7 +1,14 @@
+import { useEffect } from 'react';
 import { Route } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { getExactCourse } from '../../store/courseCard/action';
 
 export const CourseCard = ({ courseId }) => {
+    const dispatch = useDispatch();
 
+    useEffect(() => {
+        dispatch(getExactCourse(courseId));
+    }, []);
 
     return (
         <Route exact path={`/courses/${courseId}`}>
