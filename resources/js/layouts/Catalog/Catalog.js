fb717fb2fe14f3df3fc9778d86ddf1f51870fdd3
cricket <rakhaevpavel@gmail.com>
2021-10-26 01:21:59 +0300

    Корректировка импортов и объявление TODO

--- resources/js/layouts/Catalog/Catalog.js
+++ resources/js/layouts/Catalog/Catalog.js
@@ -1,21 +1,20 @@
 import { useCallback, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useParams, Route, Switch } from 'react-router-dom';
+import { Grid } from '@mui/material';
+import { Box } from '@mui/system';
 import { selectCourseList, selectFilteredList, selectIsFiltered } from '../../store/courseList/selectors';
 import { Course } from '../../components/Course/Course';
 import { courseListFilter, getCourseList } from '../../store/courseList/action';
 import { CourseFilter } from '../../components/CourseFilter/CourseFilter';
-import style from './Catalog.module.scss';
-import { Grid } from '@mui/material';
-import { Box } from '@mui/system';
-import { useParams, Route, Switch } from 'react-router-dom';
 import { CourseCard } from '../../components/CourseCard/CourseCard';
+import style from './Catalog.module.scss';
 
 export const Catalog = () => {
     const courseList = useSelector(selectCourseList);
     const filteredList = useSelector(selectFilteredList);
     const isFiltered = useSelector(selectIsFiltered);
     const { courseId } = useParams();
-
     const dispatch = useDispatch();
 
     const handleFilter = useCallback((value) => {
@@ -29,6 +28,7 @@ export const Catalog = () => {
     return (
         <Switch>
             <Route exact={true} path="/courses">
+                {/*TODO: Сформировать отдельный компонент CourseList*/}
                 <section className={style.section}>
                     <div className={`${style.section__wrap} container`}>
                         <Box sx={{ flexGrow: 1, display: 'flex' }}>
