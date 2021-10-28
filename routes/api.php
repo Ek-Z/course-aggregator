4505b2b32ebdc7c5f8d9959827c5865cacf30748
Ek-Z <74703154+Ek-Z@users.noreply.github.com>
2021-10-28 19:56:37 +0300

    added login, register, logout

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,8 @@ use App\Http\Controllers\Admin\CoursesController as AdminCoursesController;
 use App\Http\Controllers\Admin\ProgrammingLanguagesController as AdminProgrammingLanguagesController;
 use App\Http\Controllers\CoursesController;
 use App\Http\Controllers\ProgrammingLanguagesController;
-use Illuminate\Http\Request;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,27 +19,33 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+//Protected routes
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('logout', [LoginController::class, 'logout']);
+    Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
+        Route::apiResources([
+            'courses' => AdminCoursesController::class,
+        ]);
+        Route::apiResources([
+            'programmingLanguages' => AdminProgrammingLanguagesController::class,
+        ]);
+    });
 });
 
-Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
-    Route::apiResources([
-        'courses' => AdminCoursesController::class,
-    ]);
-    Route::apiResources([
-        'programmingLanguages' => AdminProgrammingLanguagesController::class,
-    ]);
+//Public routes
+
+Route::post('register', [RegisterController::class, 'register']);
+Route::post('login', [LoginController::class, 'login']);
+
+Route::group(['prefix' => 'courses', 'as' => 'courses.'], function () {
+    Route::get('/', [CoursesController::class, 'index'])
+        ->name('index');;
+    Route::get('/{id}', [CoursesController::class, 'show'])
+        ->name('show');
+    Route::get('/search/{title}', [CoursesController::class, 'search'])
+        ->name('search');
 });
-
-Route::get('/courses', [CoursesController::class, 'index'])
-    ->name('courses.index');
-
-Route::get('/courses/{id}', [CoursesController::class, 'show'])
-    ->name('courses.show');
 
 Route::get('/programmingLanguages', [ProgrammingLanguagesController::class, 'index'])
     ->name('programmingLanguages.index');
-
-Route::get('/courses/search/{title}', [CoursesController::class, 'search'])
-    ->name('courses.search');
