6fec4f9226c1879c3624b135611dd050fff786b2
Ek-Z <74703154+Ek-Z@users.noreply.github.com>
2021-10-31 18:59:21 +0300

    Revert "Update web.php"
    This reverts commit 73372cceb7287bc429dcb3fbcfa053770ef88090.

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    //return view('welcome');
-    return ('Hello!');
+    return view('welcome');
 });
 
 //Auth::routes();
