a7155903022cd664b5ed3a5c10626ecc19d3d7ec
Ek-Z <74703154+Ek-Z@users.noreply.github.com>
2021-10-31 15:10:54 +0300

    update

--- resources/js/components/Router/Router.js
+++ resources/js/components/Router/Router.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import { HashRouter, Route, Switch, useParams } from 'react-router-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 import { Header } from '../../layouts/Header/Header';
 import { Catalog } from '../../layouts/Catalog/Catalog';
 import { AdminPanel } from '../../layouts/AdminPanel/AdminPanel';
 import {LoginPage} from "../../layouts/LoginPage/LoginPage";
 import {RegistrationPage} from "../../layouts/RegistrationPage/RegistrationPage";
-import { CourseCard } from '../CourseCard/CourseCard';
 
 export const Router = () => {
     return (
@@ -14,11 +13,8 @@ export const Router = () => {
             <Switch>
                 <Route exact={true} path="/">
                 </Route>
-                <Route exact={true} path="/courses">
-                    <Catalog/>
-                </Route>
                 <Route exact={true} path="/courses/:courseId?">
-                    <CourseCard/>
+                    <Catalog/>
                 </Route>
                 <Route exact={true} path="/admin">
                     <AdminPanel/>
--- resources/js/layouts/Catalog/Catalog.js
+++ resources/js/layouts/Catalog/Catalog.js
@@ -1,24 +1,36 @@
-import { useEffect } from 'react';
+import { useCallback, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useParams, Route, Switch } from 'react-router-dom';
 import { selectCourseList } from '../../store/courseList/selectors';
-import { getPublicCourseList, getAdminCourseList } from '../../store/courseList/action';
+import { courseListFilter, getCourseList } from '../../store/courseList/action';
+import { CourseCard } from '../../components/CourseCard/CourseCard';
 import { CourseList } from '../../components/CourseList/CourseList';
 import { CourseFilter } from '../../components/CourseFilter/CourseFilter';
 
 export const Catalog = () => {
     const courseList = useSelector(selectCourseList);
+    const { courseId } = useParams();
     const dispatch = useDispatch();
 
+    const handleFilter = useCallback((value) => {
+        dispatch(courseListFilter(value, courseList));
+    });
+
     useEffect(() => {
-        //TODO: добавить проверку на админа
-        /*dispatch(getPublicCourseList());*/
-        dispatch(getAdminCourseList());
+        dispatch(getCourseList());
     }, [dispatch]);
 
     return (
-        <div className="container" style={{ display: 'flex', marginTop: 50 }}>
-            <CourseFilter/>
-            <CourseList list={courseList}/>
-        </div>
+        <Switch>
+            <Route exact={true} path="/courses">
+                <div className="container" style={{ display: 'flex', marginTop: 50 }}>
+                    <CourseFilter/>
+                    <CourseList list={courseList}/>
+                </div>
+            </Route>
+            <Route exact={true} path={`/courses/${courseId}`}>
+                <CourseCard courseId={courseId}/>
+            </Route>
+        </Switch>
     );
 };
