8eb3b6d32d554d26f9b60b2c1fcd5f8c80a530d9
Ek-Z <74703154+Ek-Z@users.noreply.github.com>
2021-11-01 09:19:41 +0300

    Update urls.js

--- resources/js/utils/urls/urls.js
+++ resources/js/utils/urls/urls.js
@@ -1,2 +1,2 @@
-export const PUBLIC_COURSES_LIST_URL = 'http://127.0.0.1:8000/api/courses';
-export const ADMIN_COURSE_LIST_URL = 'http://127.0.0.1:8000/api/admin/courses';
+export const PUBLIC_COURSES_LIST_URL = 'https://grim-moonlight-48111.herokuapp.com/api/courses';
+export const ADMIN_COURSE_LIST_URL = 'https://grim-moonlight-48111.herokuapp.com/api/admin/courses';
