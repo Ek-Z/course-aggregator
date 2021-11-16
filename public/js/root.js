f2f935212780cc10948cef9d0f4f09775fd84b98
cricket <rakhaevpavel@gmail.com>
2021-11-16 16:55:17 +0300

    Добавлен шрифт для заголовков

--- public/js/root.js
+++ public/js/root.js
@@ -35771,7 +35771,7 @@ __webpack_require__.r(__webpack_exports__);
 
 var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0___default()(function(i){return i[1]});
 // Module
-___CSS_LOADER_EXPORT___.push([module.id, "* {\r\n    box-sizing: border-box;\r\n}\r\n\r\nbody, h1, h2, h3, p, span, a {\r\n    font-family: Roboto, sans-serif;\r\n    margin: 0;\r\n    padding: 0;\r\n}\r\n\r\nimg {\r\n    display: block;\r\n}\r\n\r\n.container {\r\n    max-width: 1140px;\r\n    margin: 0 auto;\r\n}\r\n", ""]);
+___CSS_LOADER_EXPORT___.push([module.id, "* {\n    box-sizing: border-box;\n}\n\nbody, p, span, a {\n    font-family: Roboto, sans-serif;\n    margin: 0;\n    padding: 0;\n}\n\nh1, h2, h3, h4, h5, h6 {\n    font-family: 'Playfair Display SC', sans-serif;\n    margin: 0;\n    padding: 0;\n}\n\nimg {\n    display: block;\n}\n\n.container {\n    max-width: 1140px;\n    margin: 0 auto;\n}\n", ""]);
 // Exports
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,6 +7,9 @@
         <title>IT Courses</title>
         <link href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" rel="stylesheet">
         <link rel="stylesheet" href="{{ asset('css/app.css') }}"/>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Playfair+Display+SC:wght@900&display=swap" rel="stylesheet">
     </head>
     <body>
         <div id="app"></div>
