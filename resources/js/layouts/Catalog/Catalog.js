64cbc24f968450ad317198fd7fe3f4c9f5e9730c
YuliyaMikhaleva <umihaleva93@mail.ru>
2021-11-16 10:37:58 +0500

    Добавлена пагинация на страницу каталога

--- resources/js/layouts/Catalog/Catalog.js
+++ resources/js/layouts/Catalog/Catalog.js
@@ -1,15 +1,18 @@
-import { useEffect } from 'react';
+import {useEffect, useState} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import {
     selectCourseList,
     selectCourseListLength,
     selectFilteredList,
-    selectFilterWords
+    selectFilterWords,
 } from '../../store/courseList/selectors';
 import { CourseList } from '../../components/CourseList/CourseList';
 import { CourseFilter } from '../../components/CourseFilter/CourseFilter';
-import { getPublicCourseList } from '../../store/courseList/action';
+import {getPublicCourseList} from '../../store/courseList/action';
 import InputSearch from "../../components/InputSearch/InputSearch";
+import style from "./Catalog.module.scss"
+import Pagination from "@mui/material/Pagination";
+
 
 export const Catalog = () => {
     const filterWords = useSelector(selectFilterWords);
@@ -18,9 +21,27 @@ export const Catalog = () => {
     const courseListLength = useSelector(selectCourseListLength);
     const dispatch = useDispatch();
 
+    const  [currentPage, setCurrentPage] = useState(1)//текущая страница = 1
+    const [coursesPerPage] = useState(4)//всего курсов на странице
+
+    //номер последней страницы пагинации
+    const lastPageIndex = currentPage * coursesPerPage
+    //номер первой страницы пагинации
+    const firstCourseIndex = lastPageIndex - coursesPerPage
+
+    //текущая страничка для нефильтрованного каталога (на ней высвечивается только часть из общего списка)
+    const currentCourseList= courseList.slice(firstCourseIndex,lastPageIndex)
+    //текущая страничка для отфильтрованного каталога
+    const currentFilteredList= filteredList.slice(firstCourseIndex,lastPageIndex)
+
+    //количество кнопок пагинации курсов без фильтрации
+    const countCourses= Math.ceil(courseList.length/coursesPerPage)
+    //количество кнопок пагинации курсов с фильтрацией
+    const countFilteredCourses = Math.ceil(filteredList.length/coursesPerPage)
+
     useEffect(() => {
         !courseListLength && dispatch(getPublicCourseList());
-    }, [dispatch, courseListLength]);
+    }, [dispatch]);
 
     return (
         <div className="container" style={{ display: 'flex', flexDirection: 'column', marginTop: 20 }}>
@@ -30,8 +51,13 @@ export const Catalog = () => {
             <InputSearch/>
             <div style={{ display: 'flex' }}>
                 <CourseFilter/>
-                <CourseList list={filterWords ? filteredList : courseList}/>
+                <CourseList list={filterWords ? currentFilteredList : currentCourseList}/>
             </div>
+            <Pagination className={style.pagination}
+                        key={`button-${currentPage}`} /* fixed issue */
+                        count={filterWords ? countFilteredCourses : countCourses}
+                        defaultPage={currentPage}
+                        onChange={(event, value) => setCurrentPage(value)}/>
         </div>
     );
 };
