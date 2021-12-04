22da9f6715707a1c39207d8444703b9851ea54a8
Ek-Z <74703154+Ek-Z@users.noreply.github.com>
2021-12-04 10:18:09 +0300

    Update app.php

--- config/app.php
+++ config/app.php
@@ -28,7 +28,7 @@ return [
     |
     */
 
-    'env' => env('APP_ENV', 'local'),
+    'env' => env('APP_ENV', 'production'),
 
     /*
     |--------------------------------------------------------------------------
@@ -41,7 +41,7 @@ return [
     |
     */
 
-    'debug' => (bool) env('APP_DEBUG', true),
+    'debug' => (bool) env('APP_DEBUG', false),
 
     /*
     |--------------------------------------------------------------------------
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://grim-moonlight-48111.herokuapp.com/'),
 
     'asset_url' => env('ASSET_URL', null),
 
