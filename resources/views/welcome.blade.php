c6d9e946c251b2f506630d1a9d66c5dbf81c3ba4
Ek-Z <74703154+Ek-Z@users.noreply.github.com>
2021-12-09 20:36:57 +0300

    update for local env

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,10 +5,10 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <script src="{{ secure_asset('js/root.js') }}" defer></script>
+    <script src="{{ asset('js/root.js') }}" defer></script>
     <title>IT Courses</title>
     <link href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" rel="stylesheet">
-    <link rel="stylesheet" href="{{ secure_asset('css/app.css') }}" />
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display+SC:wght@900&display=swap" rel="stylesheet">
